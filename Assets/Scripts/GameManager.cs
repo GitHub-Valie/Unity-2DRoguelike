fb7da30820eed619d5d5f1f0dc1295222784db82
ValentinLHB <valentin.lhb@gmail.com>
2023-12-10 22:44:21 +0100

    singleton pattern

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,12 +4,25 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    // Static instance of GameManager which allows it to be accessed by any other script
+    public static GameManager instance = null;
     public BoardManager boardScript;
 
     private int level = 4;
 
     void Awake()
     {
+        // Singleton pattern: a single instance of GameManager is allowed at any given time
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject); // Persistence between scenes
+
         boardScript = GetComponent<BoardManager>();
         InitGame();
     }
