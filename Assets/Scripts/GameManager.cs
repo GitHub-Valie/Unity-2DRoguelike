6934ea0bbce58b39e1c35f6e0da9cc8bde0e09c6
ValentinLHB <valentin.lhb@gmail.com>
2023-12-11 15:11:46 +0100

    food-based player movement

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,6 +7,7 @@ public class GameManager : MonoBehaviour
     // Static instance of GameManager which allows it to be accessed by any other script
     public static GameManager instance = null;
     public BoardManager boardScript;
+    public int playerFoodPoints = 100;
 
     private int level = 4;
 
@@ -30,5 +31,11 @@ public class GameManager : MonoBehaviour
     void InitGame()
     {
         boardScript.SetupScene(level);
+    }
+
+    public void GameOver()
+    {
+        Debug.Log("Game is over");
+        enabled = false;
     }
 }
