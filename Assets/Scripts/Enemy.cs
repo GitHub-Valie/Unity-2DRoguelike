7ccf9aea4c902219a7dece8528057bf6365e6416
ValentinLHB <valentin.lhb@gmail.com>
2023-12-11 23:11:30 +0100

    enemy movement

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MovingObject
+{
+
+    private Transform target; // Used to store the player's position
+    private bool skipMove;
+
+    protected override void Start()
+    {
+        // The enemy script adds itself to the list of enemies in the GameManager
+        GameManager.instance.AddEnemyToList(this);
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        base.Start(); // box collider, rb2d, inverseMoveTime
+    }
+
+    protected override void AttemptMove<T>(int xDir, int yDir)
+    // In this case, pass in the Player component which the enemy will interact with (move towards, attack)
+    {
+        if (skipMove) // If skipMove is true, set it to false and do nothing else
+        {
+            skipMove = false;
+            return; // Enemy will not attempt move this turn
+        }
+
+        base.AttemptMove<T>(xDir, yDir);
+        
+        skipMove = true; // Once the enemy attempts to move, set skipMove to true
+    }
+
+    public void MoveEnemy() // Called by the GameManager when it issues the order to move to the enemies
+    {
+        // Implement logic to move enemy
+        Debug.Log("Enemy is moving");
+
+        int xDir = 0;
+        int yDir = 0;
+
+        /* Check if the difference between the target's x-axis transform position 
+        and the enemy's x-axis transform position is less than epsilon (~0) */
+        if (Mathf.Abs(target.position.x - transform.position.x) < float.Epsilon)
+        {
+            /* If that's the case, the enemy and the target player are in the same column 
+            In that case, check if the target is above the enemy */
+            yDir = target.position.y > transform.position.y ? 1 : -1; // If so, move up 1, otherwise, move down -1
+        }
+        else
+        {
+            /* If the target and the enemy are not on the same column, move left or right, accordingly:
+            If the target is to the right of the enemy, move right 1, otherwise move left -1 */
+            xDir = target.position.x > transform.position.x ? 1 : -1;
+        }
+
+        AttemptMove<Player>(xDir, yDir); // Attempt move towards the player using xDir, yDir
+    }
+
+    // Finally, what happens if the enemy attempts to move where the player is standing?
+    protected override void OnCantMove<T>(T component) 
+    {
+        // Handles cases where the enemy is blocked by the player
+        // Implement logic to hit player and make them lose food, for now, console log and throw an error
+        Debug.Log("Enemy is attacking player");
+        throw new System.NotImplementedException();
+    }
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     // Static instance of GameManager which allows it to be accessed by any other script
+    public float turnDelay = .1f;
     public static GameManager instance = null;
     public BoardManager boardScript;
     public int playerFoodPoints = 100;
     [HideInInspector] public bool playersTurn;
 
     private int level = 4;
+    private List<Enemy> enemies; // Keeps track of enemies and their movement
+    private bool enemiesMoving;
 
     void Awake()
     {
@@ -24,13 +27,14 @@ public class GameManager : MonoBehaviour
             Destroy(gameObject);
         }
         DontDestroyOnLoad(gameObject); // Persistence between scenes
-
+        enemies = new List<Enemy>();
         boardScript = GetComponent<BoardManager>();
         InitGame();
     }
 
     void InitGame()
     {
+        enemies.Clear(); // Clear the list of enemies upon scene setup
         boardScript.SetupScene(level);
     }
 
@@ -38,5 +42,42 @@ public class GameManager : MonoBehaviour
     {
         Debug.Log("Game is over");
         enabled = false;
+    }
+
+    void Update()
+    {
+        if (playersTurn || enemiesMoving) // Return as long as it's playersTurn or enemies are moving
+        {
+            return;
+        }
+
+        // If it's not playersTurn and enemies are not moving...
+        StartCoroutine(MoveEnemies());
+    }
+
+    public void AddEnemyToList(Enemy script) // Adding enemies to the list allows the GameManager to issue orders to them
+    {
+        enemies.Add(script);
+    }
+
+    IEnumerator MoveEnemies() // Moves enemies one at a time, in sequence
+    {
+        enemiesMoving = true;
+
+        // Wait for player's turn
+        yield return new WaitForSeconds(turnDelay);
+        if (enemies.Count == 0)
+        {
+            yield return new WaitForSeconds(turnDelay);
+        }
+
+        for (int i = 0; i < enemies.Count; i++) // for each enemy in enemies list, MoveEnemy
+        {
+            enemies[i].MoveEnemy();
+            yield return new WaitForSeconds(enemies[i].moveTime); // moveTime is inherited from MovingObject
+        }
+
+        playersTurn = true;
+        enemiesMoving = false;
     }
 }
