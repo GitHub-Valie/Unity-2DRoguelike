dc4740ee2439549ec4427d11819d0f4cc0077422
ValentinLHB <valentin.lhb@gmail.com>
2023-12-15 11:26:45 +0100

    mobile controls

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -23,6 +23,11 @@ public class Player : MovingObject // Inheritance
     public AudioClip drinkSound2;
     public AudioClip gameOverSound;
 
+#if UNITY_IOS || UNITY_ANDROID || UNITY_IPHONE
+    private Vector2 touchOrigin = -Vector2.one; // Used to store the x,y location of screen where the finger first touched the screen
+
+#endif
+
     protected override void Start()
     {
         animator = GetComponent<Animator>();
@@ -46,6 +51,8 @@ public class Player : MovingObject // Inheritance
 
         int horizontal = 0;
         int vertical = 0;
+    
+#if UNITY_STANDALONE || UNITY_WEBPLAYER
 
         horizontal = (int)Input.GetAxisRaw("Horizontal");
         vertical = (int)Input.GetAxisRaw("Vertical");
@@ -54,6 +61,45 @@ public class Player : MovingObject // Inheritance
         {
             vertical = 0;
         }
+    
+#elif UNITY_IOS || UNITY_ANDROID || UNITY_IPHONE
+
+// horizontal and vertical are set in the touch-screen specific code below
+
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0); // Get a reference to the first touch
+
+            if (touch.phase == TouchPhase.Began) // If touch phase has just began ...
+            {
+                touchOrigin = touch.position; // ... Set touchOrigin equal to touch's x,y position
+            }
+
+            // If the finger has lifted off the screen and touchOrigin is within the boundaries of the touch screen
+            else if (touch.phase == TouchPhase.Ended && touchOrigin.x >= 0)
+            {
+                Vector2 touchEnd = touch.position;
+                
+                // In which direction? Calculate the difference between the begining and the end of touch
+                float xDelta = touchEnd.x - touchOrigin.x;
+                float yDelta = touchEnd.y - touchOrigin.y;
+                touchOrigin.x = -1; // Set touchOrigin.x back to -1 (off screen) so that the conditional does not repeatedly evaluate to true 
+            
+                // Was the finger movement more horizontal? more vertical?
+                if (Mathf.Abs(xDelta) > Mathf.Abs(yDelta))
+                {
+                    // Finger movement is more horizontal than vertical
+                    horizontal = xDelta > 0 ? 1 : -1; // x > 0 : move right / x < 0 : move left
+                }
+                else 
+                {
+                    // Finger movement is more vertical than it is horizontal
+                    vertical = yDelta > 0 ? 1 : -1; // y > 0 : move up / y < 0 : move down
+                }
+            }
+        }        
+
+#endif
 
         if (horizontal != 0 || vertical != 0)
         {
